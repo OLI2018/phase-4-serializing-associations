fb61c5e6b1b049f544fa59ae43b99cad0adfe587
Oleg <roxvemm2014@gmail.com>
2021-11-28 18:54:30 -0800

    initial commit

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,8 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
-  
-end
+
+  belongs_to :director
+  has_many :reviews
+end 
+
+
